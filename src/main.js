54d9181607680db963a6ed516ce78e7eea6aa2ca
Matheus H. Potrich <matheushp.com@hotmail.com>
2022-06-15 00:07:53 -0300

    fix cors

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ require('dotenv').config({ path: '/src/config/.env' });
 
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use([express.json(), (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+}]);
 
 const setStatus = require("./api/setStatus");
 const getStatus = require("./api/getStatus");
